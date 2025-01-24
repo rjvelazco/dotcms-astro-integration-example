b6336031be9b26ce6c6da6d34c4561e8c8ebf68b
Rafael Velazco <rjvelazco21@gmail.com>
2025-01-23 21:44:21 -0400

    feat: configure Astro React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
+  output: "server",
   vite: {
     plugins: [tailwindcss()],
   },
